ac96e1b41962b7d1456ee019c62dbb782ea99bc7
Arsen Tolkov <formycomputer1305@gmail.com>
2025-04-20 08:01:21 +0000

    update

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,16 +1,27 @@
+import { lazy, Suspense } from "react";
 import { Routes, Route } from "react-router-dom";
 import Navigation from "../Navigation/Navigation";
-import HomePage from "../../pages/HomePage";
-import MoviesPage from "../../pages/MoviesPage";
-import MovieDetailsPage from "../../pages/MovieDetailsPage";
-import MovieCast from "../MovieCast/MovieCast";
-import MovieReviews from "../MovieReviews/MovieReviews";
+// import HomePage from "../../pages/HomePage";
+// import MoviesPage from "../../pages/MoviesPage";
+// import MovieDetailsPage from "../../pages/MovieDetailsPage";
+// import MovieCast from "../MovieCast/MovieCast";
+// import MovieReviews from "../MovieReviews/MovieReviews";
+// import NotFoundPage404 from "../../pages/NotFoundPage.jsx"
 import css from "./App.module.css";
+
+const Loader = () => <div className="loader">Loading...</div>;
+const HomePage = lazy(() => import("../../pages/HomePage"));
+const MoviesPage = lazy(() => import("../../pages/MoviesPage"));
+const MovieDetailsPage = lazy(() => import("../../pages/MovieDetailsPage"));
+const MovieCast = lazy(() => import("../MovieCast/MovieCast"));
+const MovieReviews = lazy(() => import("../MovieReviews/MovieReviews"));
+const NotFoundPage404 = lazy(() => import("../../pages/NotFoundPage.jsx"));
 
 export default function App() {
   return (
     <div className={css.container}>
       <Navigation className={css.nav} />
+      <Suspense fallback={<Loader />} />
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/movies" element={<MoviesPage />} />
@@ -18,6 +29,7 @@ export default function App() {
           <Route path="cast" element={<MovieCast />} />
           <Route path="reviews" element={<MovieReviews />} />
         </Route>
+        <Route path="*" element={<NotFoundPage404 />}></Route>
       </Routes>
     </div>
   );
---
+++ src/pages/NotFoundPage.jsx
@@ -0,0 +1,10 @@
+import css from "./NotFoundPage.module.css"
+
+
+export default function NotFoundPage404() {
+  return (
+  <div>
+    <h3 className={css.NotFoundText}>404 Not Found!</h3>
+  </div>
+  )
+}
